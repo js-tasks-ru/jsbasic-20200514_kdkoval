646f33fb8cde48443deac03b80999a40c095bc7b
Test <test@gmail.com>
2020-05-21 17:24:25 +0300

    added factorial function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,21 @@
  * @returns {number}
  */
 function factorial(n) {
+  let result = 1;
 
+  if (n <= 1) {
+    return result;
+  }
+
+  for (let i = n; i !== 0; i --) {
+    result = i * result;
+  }
+
+  return result;
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
+
