79c12ec8e3a8f0e280ac1c01c23fb41249d1d154
Test <test@gmail.com>
2020-05-27 16:36:30 +0300

    added sum function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,22 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
-
+const salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+function sumSalary(dataEmployees) {
+  let result = 0;
+  for (const key in dataEmployees) {
+    if (typeof salaries[key] === 'number') {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
+
+
